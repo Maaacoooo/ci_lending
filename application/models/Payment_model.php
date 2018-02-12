b49ff2cedd4c5fe445c98a947cb11ab3fb618c1a
Maco Cortes <maco.techdepot@gmail.com>
2018-02-12 23:35:07 +0800

    Prepared Payments

---
+++ application/models/Payment_model.php
@@ -0,0 +1,92 @@
+<?php if (!defined('BASEPATH'))exit('No direct script access allowed');
+
+Class Payments_Model extends CI_Model {
+
+
+
+    function generateID() {
+
+        $total_rows = $this->db->count_all('payments');
+        return date('Y').'-'.prettyID(($total_rows + 1), 5);  
+    }
+
+
+    function create($loan_id, $amount, $payee, $description, $receipt, $user) {
+
+        $id = $this->generateID();
+
+        $data = array(
+            'id'          => $id,
+            'loan_id'     => $loan_id,
+            'amount'      => $amount,
+            'payee'       => $payee,            
+            'description' => $description,  
+            'loan_id'     => $receipt,
+            'loan_id'     => $loan_id,
+            'user'        => $user
+        );
+
+        $this->db->insert('payments', $data);
+
+        return $id;
+    }
+
+
+    function view($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('payments');
+
+        return $query->row_array();
+    }
+
+
+    function fetch_payments($limit, $id, $loan_id = NULL) {
+
+            
+            if(!is_null($limit)) {
+                $this->db->limit($limit, (($id-1)*$limit));
+            }
+
+            $this->db->select('
+              users.username as user,
+              users.name,
+              payments.id,
+              payments.title,
+              payments.receipt,
+              payments.amount,
+              payments.payee,
+              payments.description,
+              payments.created_at
+              ');
+            $this->db->join('users', 'users.username = payments.user', 'left');
+
+            if($loan_id) {
+                $this->db->where('loan_id', $loan_id);
+            }
+
+            $this->db->order_by('payments.created_at', 'DESC');
+
+            $query = $this->db->get("payments");
+
+            if ($query->num_rows() > 0) {
+                return $query->result_array();
+            }
+            return false;
+
+    }
+
+    
+    function count_payments($loan_id = NULL) {
+        
+         if($loan_id) {
+            $this->db->where('loan_id', $loan_id);
+        }
+        return $this->db->count_all_results("payments");
+    }
+
+
+
+   
+
+
+}
