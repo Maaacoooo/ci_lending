aa33bc7294e3cd230b65d59e6a559850b1f04a68
Maco Cortes <maco.techdepot@gmail.com>
2018-03-06 14:23:54 +0800

    updated auth

--- application/controllers/Api/Auth.php
+++ application/controllers/Api/Auth.php
@@ -41,7 +41,7 @@ class Auth extends CI_Controller {
           if ($user_info) {
             $data["error"] = FALSE;
             $data["uid"] = $user_info["pin"];
-            $data["user"]["name"] = $user_info["name"];
+            $data["user"]["name"] = $user_info["firstname"] . ' ' . $user_info["lastname"];
             $data["user"]["email"] = $user_info["email"];
             $data["user"]["created_at"] = $user_info["created_at"];
             $data["user"]["updated_at"] = $user_info["updated_at"];
@@ -58,13 +58,13 @@ class Auth extends CI_Controller {
           if ($user_info) {
             $data["error"] = FALSE;
             $data["uid"] = $user_info["pin"];
-            $data["user"]["name"] = $user_info["name"];
+            $data["user"]["name"] = $user_info["firstname"] . ' ' . $user_info["lastname"];
             $data["user"]["email"] = $user_info["email"];
             $data["user"]["created_at"] = $user_info["created_at"];
             $data["user"]["updated_at"] = $user_info["updated_at"];
           } else {
             $data['error'] = TRUE;
-            $data['error_msg'] = 'Login Failed. No User Found!';
+            $data['error_msg'] = 'Login Failed. No User Found in the System!';
           }
         }
 
@@ -74,7 +74,7 @@ class Auth extends CI_Controller {
       }
 
 
-      echo json_encode($data, JSON_PRETTY_PRINT);
+      echo json_encode($data);
 
   }
 
@@ -125,6 +125,42 @@ class Auth extends CI_Controller {
       echo json_encode($data, JSON_PRETTY_PRINT);
 
   }
+
+
+  function getAllBorrowers($api_key = null) {
+
+      $data = NULL;
+
+      //change of request
+      if(isset($_GET['api_key'])) {
+        $api_key = $_GET['api_key'];
+      }
+
+      //fetch API KEY AUTH data
+      $api = $this->settings_model->setting('API_KEY_AUTH');
+
+      if($api_key == $api['value']) {
+
+        
+        $borrowers = $this->borrower_model->fetch_borrowers(NULL, NULL, NULL, 0);
+        if ($borrowers) {
+          foreach ($borrowers as $bor) {
+            $temp['client_name'] = $bor['name'];
+            $temp['client_id'] = $bor['id'];
+            $data[] = $temp;
+          }
+        }
+
+
+      } else {
+        show_error('Please contact the System Administrator!',403,'Access Denied!');
+      }
+
+
+      echo json_encode($data, JSON_PRETTY_PRINT);
+
+  }
+
 
 
 }
